56de2e6890533341d4e817fa188922244bad9281
Jake Dillon <jakedillon@macbook-pro-60.local>
2018-08-20 14:30:44 -0500

    Initial Commit

---
+++ Model a car/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Model a car
+//
+//  Created by Jake Dillon on 8/20/18.
+//  Copyright © 2018 Jake Dillon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
